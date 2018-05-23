404857206f6de350bcb83169b4beb4ea318c1ebf
PhenixR <963607063@qq.com>
2018-05-23 22:09:54 +0800

    carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,30 +1,31 @@
 window.onload = function() {
-    var slideBox = document.querySelector('.slide_box');
+    var slideCar = document.querySelector('.carousel');
     var slideItem = document.querySelector('.slide_item');
     var slideItems = document.querySelectorAll('.slide_item');
+    var slideBox = document.querySelector('.slide_box');
     var nextd = document.querySelector('.next');
     var pred = document.querySelector('.pre');
     var offLeft = slideItem.offLeft;
     var len = slideItems.length;
     var itemWidth = slideItem.clientWidth;
-    setBoxWidth();
-    
-    function setBoxWidth() {
-        slideBox.style.width = itemWidth + "px";
+    var itemH = slideItem.clientHeight;
+    setinit();
+    function setinit() {
+        slideCar.style.width = itemWidth + "px";
+        slideCar.style.height = itemH + "px";
     }
-
     function slideN() {
-        if (slideItem.offsetLeft < (len-1)*itemWidth) {
-            slideItem.style.left = 0 + "px";
+        if (slideBox.offsetLeft < -(len-2)*itemWidth) {
+            slideBox.style.left = 0 + "px";
         } else {
-            slideItem.style.left = offLeft + itemWidth + "px"
+            slideBox.style.left = slideBox.offsetLeft - itemWidth + "px"
         }
     }
     function slideP() {
-        if (slideItem.offsetLeft > 0) {
-            slideItem.style.left = (len-1)*itemWidth + "px"
+        if (slideBox.offsetLeft > -itemWidth ) {
+            slideBox.style.left = -(len-1)*itemWidth + "px"
         } else {
-            slideItem.style.left = offLeft - itemWidth + "px"
+            slideBox.style.left = slideBox.offsetLeft + itemWidth + "px"
         }
     }
     nextd.onclick = function() {
